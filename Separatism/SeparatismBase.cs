0a98d0d16cbaa609d6e01105e263b51b726e5c33
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-04-24 23:22:45 +0400

    remove unnecessary dependencies and add mod readme

--- Separatism/SeparatismBase.cs
+++ Separatism/SeparatismBase.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using TaleWorlds.CampaignSystem;
+﻿using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
-using TaleWorlds.SaveSystem;
-using TaleWorlds.CampaignSystem.Actions;
-using TaleWorlds.InputSystem;
 
 namespace Separatism
 {
