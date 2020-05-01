cd3aa916601327ba64ff8472cf7289935d876c44
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-05-01 18:49:28 +0400

    ability to recruit free non minor clans

---
+++ Separatism/Patches/DiplomaticBartersBehaviorPatch.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors.BarterBehaviors;
+
+namespace Separatism.Patches
+{
+	[HarmonyPatch(typeof(DiplomaticBartersBehavior), "ConsiderClanJoin")]
+	public static class ConsiderClanJoinPatch
+	{
+		public static bool Prefix(Clan clan, Kingdom kingdom)
+		{
+			if (clan.Settlements.Count() > 0 && clan.Leader.IsEnemy(kingdom.Leader))
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(DiplomaticBartersBehavior), "ConsiderClanLeaveKingdom")]
+	public static class ConsiderClanLeaveKingdomPatch
+	{
+		public static bool Prefix(Clan clan)
+		{
+			if (clan.Settlements.Count() > 0)
+			{
+				if (clan.Leader == clan.Kingdom.Leader ||
+					clan.Leader.HasGoodRelationWith(clan.Kingdom.Leader))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(DiplomaticBartersBehavior), "ConsiderDefection")]
+	public static class ConsiderDefectionPatch
+	{
+		public static bool Prefix(Clan clan1, Kingdom kingdom)
+		{
+			if (clan1.Settlements.Count() > 0)
+			{
+				if (clan1.Kingdom == kingdom ||
+					clan1.Leader == clan1.Kingdom.Leader ||
+					clan1.Leader.HasGoodRelationWith(clan1.Kingdom.Leader) ||
+					clan1.Leader.IsEnemy(kingdom.Leader) ||
+					!clan1.CloseKingdoms().Contains(kingdom))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Separatism/Patches/LordDefectionCampaignBehaviorPatch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TaleWorlds.Localization;
+
+namespace Separatism
+{
+	[HarmonyPatch(typeof(LordDefectionCampaignBehavior), "AddLordDefectionPersuasionOptions")]
+	public static class Patch
+	{
+		public static void Postfix(CampaignGameStarter starter)
+		{
+			starter.AddPlayerLine(
+				"player_is_requesting_fallen_to_join",
+				"lord_talk_speak_diplomacy_2",
+				"persuasion_leave_faction_npc",
+				"{=3gbgjJfZ}I have heard you are in search of a new sovereign, {FIRST_NAME}...",
+				new ConversationSentence.OnConditionDelegate(
+					conversation_player_is_asking_to_recruit_fallen_on_condition), null, 100, null, null);
+		}
+
+		public static bool conversation_player_is_asking_to_recruit_fallen_on_condition()
+		{
+			if (Hero.MainHero.Clan.Kingdom != null &&
+				Hero.MainHero == Hero.MainHero.Clan.Kingdom.Ruler &&
+				Hero.OneToOneConversationHero != null && 
+				Hero.OneToOneConversationHero.Clan.Kingdom == null &&
+				!Hero.OneToOneConversationHero.Clan.IsMinorFaction &&
+				Hero.OneToOneConversationHero == Hero.OneToOneConversationHero.MapFaction.Leader &&
+				!FactionManager.IsAtWarAgainstFaction(Hero.OneToOneConversationHero.MapFaction, Hero.MainHero.MapFaction))
+			{
+				Hero.OneToOneConversationHero.MapFaction.Leader.SetTextVariables();
+				MBTextManager.SetTextVariable("FACTION_NAME", Hero.MainHero.MapFaction.Name, false);
+				return true;
+			}
+			return false;
+		}
+	}
+}
