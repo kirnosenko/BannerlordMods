0c03d93cd276e2091f170450afec43f9fd4070bb
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-05-08 21:22:22 +0400

    fixed a bug of the game with deleting of periodic events that sometimes causes a crash

---
+++ Separatism/Patches/CampaignPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+
+namespace Separatism.Patches
+{
+	[HarmonyPatch(typeof(Campaign), "DeletePeriodicEvent")]
+	public static class CampaignPatch
+	{
+		public static bool Prefix(MBCampaignEvent campaignEvent)
+		{
+			return campaignEvent != null;
+		}
+	}
+}
