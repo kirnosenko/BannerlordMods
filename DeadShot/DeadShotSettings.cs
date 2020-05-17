49cf1fd61d14944df322a2b9e82a3558a26c9940
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-05-17 22:37:04 +0400

    initial dead shot mod

---
+++ DeadShot/DeadShotSettings.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using MBOptionScreen.Attributes.v2;
+using MBOptionScreen.Settings;
+
+namespace DeadShot
+{
+	public class DeadShotSettings : AttributeSettings<DeadShotSettings>
+	{
+		private static string modVersion;
+		private static string modName;
+		private static string modId;
+		
+		static DeadShotSettings()
+		{
+			modVersion = ModuleInfo.GetModules().SingleOrDefault(x => x.Name == nameof(DeadShot))?.Version.ToString() ?? string.Empty;
+			modName = $"{new TextObject($"{nameof(DeadShot)} Mod").ToString()} {modVersion}";
+			modId = $"{nameof(DeadShot)}_{modVersion}";
+		}
+		public DeadShotSettings()
+		{
+			Id = modId;
+		}
+
+		public override string ModName => modName;
+
+		public override string ModuleFolderName => nameof(DeadShot);
+
+		public override string Id { get; set; }
+
+		[SettingPropertyFloatingInteger(displayName: "Slow Motion Factor", minValue: 0.1f, maxValue: 1, HintText = "Set low value to make the effect stronger.", Order = 0, RequireRestart = false)]
+		public float SlowMotionFactor { get; set; } = 0.4f;
+	}
+}
---
+++ DeadShot/DeadShotSubModule.cs
@@ -0,0 +1,28 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace DeadShot
+{
+	public class DeadShotSubModule : MBSubModuleBase
+	{
+		protected override void OnApplicationTick(float dt)
+		{
+			if (Agent.Main != null && Agent.Main.IsActive())
+			{
+				if (Agent.Main.WieldedWeapon.IsAnyRanged(out var ranged))
+				{
+					var currentStage = Agent.Main.GetCurrentActionStage(1);
+
+					if (currentStage == Agent.ActionStage.AttackReady || currentStage == Agent.ActionStage.AttackRelease)
+					{
+						Mission.Current.Scene.SlowMotionFactor = DeadShotSettings.Instance.SlowMotionFactor;
+						Mission.Current.Scene.SlowMotionMode = true;
+					}
+					else
+					{
+						Mission.Current.Scene.SlowMotionMode = false;
+					}
+				}
+			}
+		}
+	}
+}
