d326c7a572049035c2b0e4fa50bb53c5b0b3b520
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-06-07 17:19:12 +0400

    dialog option without patch

--- Separatism/Behaviours/UtilityBehaviour.cs
+++ Separatism/Behaviours/UtilityBehaviour.cs
@@ -10,12 +10,40 @@ namespace Separatism.Behaviours
 	{
 		public override void RegisterEvents()
 		{
+			CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, OnSessionLaunched);
 			CampaignEvents.OnGameLoadedEvent.AddNonSerializedListener(this, OnGameLoaded);
 			CampaignEvents.DailyTickEvent.AddNonSerializedListener(this, OnDailyTick);
 		}
 
 		public override void SyncData(IDataStore dataStore)
 		{
+		}
+
+		private void OnSessionLaunched(CampaignGameStarter game)
+		{
+			game.AddPlayerLine(
+				"player_is_requesting_fallen_to_join",
+				"lord_talk_speak_diplomacy_2",
+				"persuasion_leave_faction_npc",
+				"{=Separatism_Clan_Recruit}{FIRST_NAME}, I have heard you are in search of a new sovereign...",
+				new ConversationSentence.OnConditionDelegate(
+					conversation_player_is_asking_to_recruit_fallen_on_condition), null, 100, null, null);
+		}
+
+		private bool conversation_player_is_asking_to_recruit_fallen_on_condition()
+		{
+			if (Hero.MainHero.Clan.Kingdom != null &&
+				Hero.MainHero == Hero.MainHero.Clan.Kingdom.Ruler &&
+				Hero.OneToOneConversationHero != null &&
+				Hero.OneToOneConversationHero.Clan.Kingdom == null &&
+				!Hero.OneToOneConversationHero.Clan.IsMinorFaction &&
+				Hero.OneToOneConversationHero == Hero.OneToOneConversationHero.MapFaction.Leader &&
+				!FactionManager.IsAtWarAgainstFaction(Hero.OneToOneConversationHero.MapFaction, Hero.MainHero.MapFaction))
+			{
+				Hero.OneToOneConversationHero.SetTextVariables();
+				return true;
+			}
+			return false;
 		}
 
 		private void OnGameLoaded(CampaignGameStarter game)
