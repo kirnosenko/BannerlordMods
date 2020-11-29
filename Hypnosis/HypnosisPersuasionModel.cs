6254eb4f18f15c08b7d5c55a35b58af6d5881b5f
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-11-29 12:51:16 +0400

    a simple mod to get 100% persuasion chance

---
+++ Hypnosis/HypnosisPersuasionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TaleWorlds.CampaignSystem.Conversation.Persuasion;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+namespace Hypnosis
+{
+	public class HypnosisPersuasionModel : DefaultPersuasionModel
+	{
+		public override void GetChances(PersuasionOptionArgs optionArgs, out float successChance, out float critSuccessChance, out float critFailChance, out float failChance, float difficultyMultiplier)
+		{
+			successChance = 1;
+			critSuccessChance = 0;
+			failChance = 0;
+			critFailChance = 0;
+		}
+		public override void GetEffectChances(PersuasionOptionArgs option, out float moveToNextStageChance, out float blockRandomOptionChance, float difficultyMultiplier)
+		{
+			moveToNextStageChance = 1;
+			blockRandomOptionChance = 0;
+		}
+	}
+}
---
+++ Hypnosis/HypnosisSubModule.cs
@@ -0,0 +1,18 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Conversation.Persuasion;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Hypnosis
+{
+	public class HypnosisSubModule : MBSubModuleBase
+	{
+		protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+		{
+			if (game.GameType is Campaign)
+			{
+				gameStarterObject.AddModel(new HypnosisPersuasionModel());
+			}
+		}
+	}
+}
