35d41af9307c7bd68d03eb4ebf834e1867273d97
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2021-08-15 16:12:12 +0400

    fixed null party crash

---
+++ Telepathy/Patches/FlattenedTroopRosterPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+
+namespace Telepathy.Patches
+{
+	[HarmonyPatch(typeof(FlattenedTroopRoster), "GenerateUniqueNoFromParty")]
+	public class FlattenedTroopRosterPatch
+	{
+		public static bool Prefix(MobileParty party, int troopIndex, ref int __result)
+		{
+			__result = ((party?.Party?.Index ?? 1) * 999983 + troopIndex * 100003) % 616841;
+
+			return false;
+		}
+	}
+}
